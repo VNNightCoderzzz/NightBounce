8f07db39559529bd90190238b2264223fbfc243f
VNNightCoderzzz <hahigamingchichi@gmail.com>
2025-08-08 14:35:18 +0700

    Add files via upload

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/PotionSaver.kt
@@ -0,0 +1,20 @@
+package net.ccbluex.liquidbounce.features.module.modules.player
+
+import net.ccbluex.liquidbounce.event.PacketEvent
+import net.ccbluex.liquidbounce.event.handler
+import net.ccbluex.liquidbounce.features.module.Category
+import net.ccbluex.liquidbounce.features.module.Module
+import net.minecraft.network.play.client.C03PacketPlayer
+
+object PotionSaver : Module("PotionSaver", Category.PLAYER) {
+
+    val onPacket = handler<PacketEvent> {
+        val packet = it.packet
+
+        if (packet is C03PacketPlayer && mc.thePlayer?.isUsingItem == false && !packet.rotating &&
+            (!packet.isMoving || (packet.x == mc.thePlayer.lastTickPosX && packet.y == mc.thePlayer.lastTickPosY && packet.z == mc.thePlayer.lastTickPosZ))
+        )
+            it.cancelEvent()
+    }
+
+}
