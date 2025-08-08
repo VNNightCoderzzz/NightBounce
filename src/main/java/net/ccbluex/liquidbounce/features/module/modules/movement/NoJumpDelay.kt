bbac6373b47686f7263e72dc4dc640b7837b130d
VNNightCoderzzz <hahigamingchichi@gmail.com>
2025-08-08 14:30:34 +0700

    Add files via upload

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/NoJumpDelay.kt
@@ -0,0 +1,6 @@
+package net.ccbluex.liquidbounce.features.module.modules.movement
+
+import net.ccbluex.liquidbounce.features.module.Category
+import net.ccbluex.liquidbounce.features.module.Module
+
+object NoJumpDelay : Module("NoJumpDelay", Category.MOVEMENT, gameDetecting = false)
