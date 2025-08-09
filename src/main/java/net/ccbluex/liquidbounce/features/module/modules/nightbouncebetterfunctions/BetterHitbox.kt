1a6700942929c018156312a533c8c8bd4b7e03c5
VNNightCoderzzz <hahigamingchichi@gmail.com>
2025-08-10 00:48:13 +0700

    Update BetterHitbox.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/nightbouncebetterfunctions/BetterHitbox.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/nightbouncebetterfunctions/BetterHitbox.kt
@@ -4,6 +4,14 @@ import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.modules.misc.AntiBot.isBot
 import net.ccbluex.liquidbounce.features.module.modules.misc.Teams
+import net.ccbluex.liquidbounce.features.module.modules.combat.*
+import net.ccbluex.liquidbounce.features.module.modules.exploit.*
+import net.ccbluex.liquidbounce.features.module.modules.fun.*
+import net.ccbluex.liquidbounce.features.module.modules.misc.*
+import net.ccbluex.liquidbounce.features.module.modules.movement.*
+import net.ccbluex.liquidbounce.features.module.modules.player.*
+import net.ccbluex.liquidbounce.features.module.modules.render.*
+import net.ccbluex.liquidbounce.features.module.modules.world.*
 import net.ccbluex.liquidbounce.utils.extensions.*
 import net.minecraft.entity.Entity
 import net.minecraft.entity.boss.EntityDragon
