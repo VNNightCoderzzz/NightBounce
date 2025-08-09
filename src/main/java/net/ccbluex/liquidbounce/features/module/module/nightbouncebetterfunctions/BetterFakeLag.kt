d2bf4772a48e623366931e252d61939b1d8f4d12
VNNightCoderzzz <hahigamingchichi@gmail.com>
2025-08-09 23:44:05 +0700

    Update BetterFakeLag.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/module/nightbouncebetterfunctions/BetterFakeLag.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/module/nightbouncebetterfunctions/BetterFakeLag.kt
@@ -34,7 +34,7 @@ import java.awt.Color
 import java.util.*
 import kotlin.math.min
 
-object FakeLag : Module("FakeLag", Category.COMBAT, gameDetecting = false) {
+object BetterFakeLag : Module("BetterFakeLag", Category.NIGHTBOUNCEBETTERFUNCTIONS, gameDetecting = false) {
 
     private val delay by int("Delay", 550, 0..1000)
     private val recoilTime by int("RecoilTime", 750, 0..2000)
