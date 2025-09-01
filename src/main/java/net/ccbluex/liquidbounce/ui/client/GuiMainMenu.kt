59118dfa1720bc27c30328c6cbac74bede201d0a
VNNightCoderzzz <hahigamingchichi@gmail.com>
2025-09-01 16:20:42 +0700

    Update GuiMainMenu.kt

--- src/main/java/net/ccbluex/liquidbounce/ui/client/GuiMainMenu.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/client/GuiMainMenu.kt
@@ -293,7 +293,7 @@ class GuiMainMenu : AbstractScreen() {
         }
 
         mc.fontRendererObj.drawStringWithShadow(
-            "b1.3.2", 10f, 10f, 0xFFFFFF
+            "b1.3.3", 10f, 10f, 0xFFFFFF
 
         )
         val liquidBounceTitle = "NightBounce"
@@ -403,6 +403,7 @@ class GuiMainMenu : AbstractScreen() {
 
 
 }
+
 
 
 
